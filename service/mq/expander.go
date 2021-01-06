020fe25f543a2f81f4e384311e07876d620de9a0
zingphoy <zingphoyhan@gmail.com>
2021-01-06 21:57:17 +0800

    feat: add asyn timeline insertion

--- service/mq/expander.go
+++ service/mq/expander.go
@@ -23,60 +23,64 @@ import (
  It's a transaction to expand data to ensure the correctness of data.
 */
 
-// fork一个子进程乱来进行消息消费，这里只负责管理子进程。
-// 检查子进程的健康度，挂了要重启（子进程挂了是不是会丢消息？）
-
 // go协程来消费消息，外围管理这个协程的状态即可，
 // InitExpander initialize the manager which will take care of go routine that consumes messages.
-func InitExpander() error {
-	err := initManager()
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	return err
+func InitExpander() {
+	initManager()
 }
 
-// todo, how to manager go routine
-func initManager() error {
-	return nil
+func initManager() {
+	c := make(chan int, 0)
+	go Expand(c)
+
+	// keep Expander alive
+	go func(chan int) {
+		for {
+			select {
+			case <-c:
+				go Expand(c)
+			}
+		}
+	}(c)
 }
 
 // Expand consume messages in MQ, expand data to other collections
-func Expand() error {
-	err := ConsumeMessage(TOPIC, callback)
+func Expand(c chan int) {
+	err := RunMessageConsumer(TOPIC, callback)
 	if err != nil {
 		log.Error("consume message failed,", err.Error())
+		c <- 1
 	}
-	return err
 }
 
 func callback(ctx context.Context, msgs ...*primitive.MessageExt) (consumer.ConsumeResult, error) {
-	// todo: subscribe callback
-	msg := Message{}
-	log.Info("message length: ", len(msgs))
+	for _, msg := range msgs {
+		// only one message at a time
+		m := Message{}
+		err := json.Unmarshal(msg.Message.Body, &m)
+		if err != nil {
+			log.Error("message deserialize failed,", err.Error())
+			return consumer.ConsumeSuccess, err
+		}
 
-	// only one message at a time
-	err := json.Unmarshal(msgs[0].Message.Body, &msg)
-	if err != nil {
-		log.Error("message deserialize failed,", err.Error())
-		return consumer.ConsumeRetryLater, err
-	}
+		aid, uid := m.Aid, m.Uid
+		if aid <= 0 || uid <= 0 {
+			log.Error(fmt.Sprintf("aid: %d, uid: %d", aid, uid))
+			return consumer.ConsumeSuccess, errors.New("aid or uid is not valided")
+		}
 
-	aid, uid := msg.Aid, msg.Uid
-	if aid <= 0 || uid <= 0 {
-		log.Error(fmt.Sprintf("aid: %d, uid: %d", aid, uid))
-		return consumer.ConsumeSuccess, errors.New("aid or uid is not valided")
-	}
-
-	switch msg.MsgType {
-	case 1:
-		err = model.AppendTimeline(uid, aid)
-	case 2:
-		err = model.RemoveTimeline(uid, aid)
-	}
-	if err != nil {
-		log.Error("expand timeline failed ")
-		return consumer.ConsumeRetryLater, err
+		// todo 获取好友列表，然后遍历调用 AppendTimeline
+		switch m.MsgType {
+		case 1:
+			// 从friend中查库出来，所以需要friend先提供接口
+			err = model.AppendTimeline(uid, aid)
+		case 2:
+			err = model.RemoveTimeline(uid, aid)
+		}
+		if err != nil {
+			log.Error("expand timeline failed ")
+			return consumer.ConsumeRetryLater, err
+		}
 	}
 	return consumer.ConsumeSuccess, nil
 }
--- service/mq/producer.go
+++ service/mq/producer.go
@@ -1,40 +1,37 @@
 package mq
 
 import (
+	"Moments/model"
 	"Moments/pkg/log"
 	"encoding/json"
+	"fmt"
+	"sync"
+
+	"github.com/apache/rocketmq-client-go/v2/primitive"
 )
 
 /*
- Producer is a service to send message into MQ
+ Producer is a module to wrap local transactions
 */
-
-// 这里是不是只需要吧消息send过去就好了？那看起来直接在原service中调用SendMessage不就行了吗？这个文件看起来没有存在的必要
 
 // ExpandTimeline a encapsulation of sending message to MQ
 func (m *Message) ExpandTimeline() error {
-	var err error
-	switch m.MsgType {
-	case 1:
-		err = SendMsg(m)
-	case 2:
-		err = SendMsg(m)
-	}
+	err := sendMsg(m)
 	if err != nil {
 		log.Error("send message to MQ failed,", err.Error())
-		return err
 	}
-	return nil
+	return err
 }
 
-// SendMsg a wrapper of sending message determine by Message.NeedSafe
-func SendMsg(m *Message) error {
+// sendMsg a wrapper of sending message determine by Message.NeedSafe
+func sendMsg(m *Message) error {
 	msg, err := json.Marshal(&m)
 	if err != nil {
 		log.Error("serialize message failed,", err.Error())
 		return err
 	}
 
+	// todo need sending to mq safe or not
 	switch m.NeedSafe {
 	case true:
 		err = SendMessage(TOPIC, msg)
@@ -42,4 +39,73 @@ func SendMsg(m *Message) error {
 		err = SendMessage(TOPIC, msg)
 	}
 	return err
+}
+
+/*
+ about local transaction below
+*/
+type TransactionListener struct {
+	localTrans *sync.Map
+}
+
+func NewTransactionListener() *TransactionListener {
+	return &TransactionListener{localTrans: new(sync.Map)}
+}
+
+/*
+ perform local transaction, using map localTrans to determine the status of message processing
+ localTrans 1:Commit 2:Rollback 3:Unknown
+*/
+func (l *TransactionListener) ExecuteLocalTransaction(msg *primitive.Message) primitive.LocalTransactionState {
+	log.Info(fmt.Sprintf("begin local transaction, transactionID: %v\n", msg.TransactionId))
+	var body Message
+	err := json.Unmarshal(msg.Body, &body)
+	if err != nil {
+		log.Error(err.Error())
+		l.localTrans.Store(msg.TransactionId, primitive.LocalTransactionState(2))
+		return primitive.RollbackMessageState
+	}
+
+	// local transaction, first append sender's timeline, then append to friends' timeline asynchronously
+	switch body.MsgType {
+	case EXPAND_TIMELINE_ADD:
+		err = model.AppendTimeline(body.Uid, body.Aid)
+	case EXPAND_TIMELINE_DELETE:
+		// todo not implement yet
+		break
+	}
+	if err != nil {
+		log.Error(err.Error())
+		l.localTrans.Store(msg.TransactionId, primitive.LocalTransactionState(2))
+		return primitive.RollbackMessageState
+	}
+
+	l.localTrans.Store(msg.TransactionId, primitive.LocalTransactionState(1))
+	return primitive.CommitMessageState
+}
+
+// to check the 'Prepared-State' transaction using strategy of TransactionListener
+func (l *TransactionListener) CheckLocalTransaction(msg *primitive.MessageExt) primitive.LocalTransactionState {
+	log.Info(fmt.Sprintf("checking local transaction, transactionID : %v\n", msg.TransactionId))
+	v, existed := l.localTrans.Load(msg.TransactionId)
+	if !existed {
+		return primitive.CommitMessageState
+	}
+
+	state := v.(primitive.LocalTransactionState)
+	l.localTrans.Delete(msg.TransactionId)
+	switch state {
+	case 1:
+		log.Info(fmt.Sprintf("checkLocalTransaction COMMITE_MESSAGE: %v\n", msg))
+		return primitive.CommitMessageState
+	case 2:
+		log.Info(fmt.Sprintf("checkLocalTransaction ROLLBACK_MESSAGE: %v\n", msg))
+		return primitive.RollbackMessageState
+	case 3:
+		log.Info(fmt.Sprintf("checkLocalTransaction UNKNOWN_MESSAGE: %v\n", msg))
+		return primitive.UnknowState
+	default:
+		log.Error(fmt.Sprintf("message state not expected, checkLocalTransaction: %v\n", msg))
+		return primitive.RollbackMessageState
+	}
 }
